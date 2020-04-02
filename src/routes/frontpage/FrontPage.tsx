981fe09b1688a904c3f55ebd3eccb98a3e82820f
Sindri Már Kaldal Sigurjónsson <tm-sindrikaldal@Sindris-MacBook-Pro.local>
2020-04-02 22:18:43 +0000

    setting up

---
+++ src/routes/frontpage/FrontPage.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+
+export const FrontPage: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>CARD GAME SITE</h1>
+      <input></input>
+    </div>
+  );
+};
